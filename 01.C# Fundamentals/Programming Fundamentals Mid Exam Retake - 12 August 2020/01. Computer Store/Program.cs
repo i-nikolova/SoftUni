5a9634a85931deb88b559d6041f9fbf09151c556
Irina Nikolova <nikolova.irina.n@gmail.com>
2020-12-02 11:24:51 +0200

    Added Programming Fundamentals Mid Exam Retake - 12 August 2020

---
+++ 01.C# Fundamentals/Programming Fundamentals Mid Exam Retake - 12 August 2020/01. Computer Store/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Problem_1._Computer_Store
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double totalSum = 0;
+            double taxes = 0;
+            double totalSumTaxes = 0;
+            string input = Console.ReadLine();
+            while (true)
+            {
+                if (input == "special" || input == "regular")
+                {
+                    break;
+                }
+                double partPrice = double.Parse(input);
+                if (partPrice<=0)
+                {
+                    Console.WriteLine("Invalid price!");
+                }
+                else
+                {
+                    totalSum += partPrice;
+                    taxes += partPrice * 0.20;
+                }
+               
+
+                input = Console.ReadLine();
+                
+            }
+            totalSumTaxes = totalSum + taxes;
+            if (input=="special")
+            {
+                totalSumTaxes *= 0.90;
+            }
+            if (totalSumTaxes==0)
+            {
+                Console.WriteLine("Invalid order!");
+            }
+            else
+            {
+                Console.WriteLine($"Congratulations you've just bought a new computer!\n" +
+                                $"Price without taxes: {totalSum:f2}$\n" +
+                                $"Taxes: { taxes:f2}$\n" +
+                                $"-----------\n" +
+                                $"Total price: {totalSumTaxes:f2}$");
+            }
+            
+        }
+    }
+}
